493bcb5a8ca57ff0c2a5d239947126e129d83208
Michael E. Nash <mnash@investcloud.com>
2016-01-31 17:12:56 -0500

    added Stripe charges to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'home#index'
   devise_for :users
+  resources :charges, :only => [:new, :create]
 end
