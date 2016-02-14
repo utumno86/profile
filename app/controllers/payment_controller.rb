757017019b7209a93479870a8b343f626a91c203
Michael E. Nash <mnash@investcloud.com>
2016-02-13 21:05:17 -0500

    updated the payment controller to support create and show actions

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,11 +1,33 @@
 # Controller for the Payment Model, which is designed to be an intermediary between the user and charges
 class PaymentController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_wrestler, :only => :show
+
   def new
   end
 
   def create
+    @payment = Payment.new(payment_params)
+    @payment.user_id = current_user.id
+    if @payment.save
+      redirect_to @payment, :notice => 'Wrestler was successfully created.'
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_payment
+    @payment = Payment.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def payment_params
+    params.require(:payment).permit(:purpose, :amount, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root to: 'home#index'
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  root :to => 'home#index'
   devise_for :users
   resources :charges, :only => [:new, :create]
   resources :payment, :only => [:new, :create, :show]
