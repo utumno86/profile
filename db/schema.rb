4846860fd87f6f03eb83ee4a35321dd50b38dad5
Michael E. Nash <mnash@investcloud.com>
2016-02-17 00:02:09 -0500

    added posts model for blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211014105) do
+ActiveRecord::Schema.define(version: 20160217045802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 20160211014105) do
   end
 
   add_index "payments", ["user_id"], name: "index_payments_on_user_id", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -48,4 +59,5 @@ ActiveRecord::Schema.define(version: 20160211014105) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "payments", "users"
+  add_foreign_key "posts", "users"
 end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :post do
+    user nil
+    title "MyString"
+    content "MyText"
+    image "MyString"
+  end
+end
