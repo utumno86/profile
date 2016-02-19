2e71eda4bbddae7746ead5720241e35271068514
Michael E. Nash <mnash@investcloud.com>
2016-02-18 20:55:00 -0500

    updates to the file uploader config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
     :region =>                'us-east-1'
   }
-  config.fog_directory  = Rails.env.development? ? 'profile-dev' : 'profile'
+  config.fog_directory  = Rails.env.development? ? 'mnashprofile-dev' : 'mnashprofile'
   config.fog_public     = false
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
 end
