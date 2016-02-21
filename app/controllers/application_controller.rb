e0ae9eb39d6ab60b099811ae6a6bef392661d5fb
Michael E. Nash <mnash@investcloud.com>
2016-02-21 16:48:53 -0500

    wrote the comments controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
+  # This is what Stack Overflow says to do to redirect in case the user is not signed in.
+  # Hope it works!
+
   protected
 
   def authenticate_user!
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,42 @@
 # document has been edited
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+
+  def create
+    set_post
+    @comment = @post.comments.create(comment_params)
+    if @comment.save
+      flash[:success] = 'Your comment was successfully added!'
+      redirect_to @post
+    end
+  end
+
+  def update
+    set_post
+    @comment = @post.comments.update(comment_params)
+    if @comment.update(comment_params)
+      flash[:success] = 'Your comment was successfully update!'
+      redirect_to @post
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      flash[:notice] = 'Your comment was successfully deleted!'
+      redirect_to :back
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def comment_params
+    params.require(:comment).permit(:content, :user_id)
+  end
 end
