63b9cd77a5bf274f16f421181d3f7ca99b4943ac
Michael E. Nash <mnash@investcloud.com>
2016-02-21 16:28:18 -0500

    added a redirect in case user is not signed in when trying to leave a comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+  protected
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to login_path, :notice => 'You must be signed in to leave a comment'
+      ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
+  protect_from_forgery :with => exception
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,6 @@
+# Although Blog Posts are controlled from the Admin panel, comments on them are controlled
+# by the user. However, they need to be signed in and something needs to indicate whetehr or not the
+# document has been edited
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
 end
