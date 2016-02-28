219c7f8ba847107f52e6a360aeae31f68116431e
Michael E. Nash <mnash@investcloud.com>
2016-02-28 12:12:08 -0500

    reconfigured environments for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.mandrillapp.com",
+    address: "smtp.sendgrid.com",
     port: 587,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
@@ -81,7 +81,7 @@ Rails.application.configure do
     password: Rails.application.secrets.email_provider_apikey
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'profile.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'mnashprofile.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
