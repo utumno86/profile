d56742f58571bc73a8a5e1bc088b5fe7ba79d770
Michael E. Nash <mnash@investcloud.com>
2016-03-01 23:42:23 -0500

    removed the wierd authenticate_user stipulation that I wasn't using anyway

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,5 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to login_path, :notice => 'You must be signed in to leave a comment'
-      ## if you want render 404 page
-      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-    end
-  end
   protect_from_forgery with: :exception
 end
