b8c7e0fc5c79a4bee7db6469fbd8770d5f2dfec8
Michael E. Nash <mnash@investcloud.com>
2016-04-21 21:23:41 -0400

    fixed my JQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
 
     // Scrolls to the selected menu item on the page
   $(function () {
-    $('a[href*=#]:not([href=#])').click(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
       if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') || location.hostname === this.hostname) {
         var target = $(this.hash)
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
