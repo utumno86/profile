694ce0be7d58e2f52ecf6b469aee84ec59ec40e2
Michael E. Nash <mnash@investcloud.com>
2016-05-11 20:03:49 -0400

    set up twitter config file

---
+++ config/twitter_config.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET_KEY']
+  config.oauth_token = ENV['TWITTER_ACCESS_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+end
